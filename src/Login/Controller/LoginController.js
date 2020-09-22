f9d82b950ee83514108426909db32830b3a2c520
Célio Paiva <celiopaivajr@gmail.com>
2020-09-21 21:29:08 -0300

    Ajusta tela login

--- src/Login/Controller/LoginController.js
+++ src/Login/Controller/LoginController.js
@@ -1,36 +1,13 @@
 import React from 'react'
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
-// import LoginView from '/'
-
+import { Button } from 'react-native-paper'
+import LoginView from '../View/LoginView'
 
 export default class LoginController extends React.Component{
 
-    render () {
+    render = () => {
         return (
-            <View style={styles.main}>
-                <View style={styles.container}>
-                    <TextInput styles={styles.input} placeholder={"Adicione o login"} type="email"></TextInput>
-                    <TextInput styles={styles.input} placeholder={"Adicione a senha"} secureTextEntry></TextInput>
-                    <TouchableOpacity style={styles.btn}>
-                        <Text>Login</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
+            <LoginView/>
         )
     }
 }
-
-const styles = StyleSheet.create({
-
-    main: {
-        height: '100%',
-        width: '100%',
-        backgroundColor: 'white'
-    },
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        marginTop: 35
-    }
-})
--- src/Login/View/LoginView.js
+++ src/Login/View/LoginView.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Text, View, TextInput, TouchableOpacity } from 'react-native'
+import { Button } from 'react-native-paper'
 import styles from './LoginStyles'
 
 const LoginView = () => {
@@ -8,11 +9,16 @@ const LoginView = () => {
             <View style={styles.container}>
                 <TextInput styles={styles.input} placeholder={"Adicione o login"} type="email"></TextInput>
                 <TextInput styles={styles.input} placeholder={"Adicione a senha"} secureTextEntry></TextInput>
-                <TouchableOpacity style={styles.btn}>
-                    <Text>Login</Text>
-                </TouchableOpacity>
+                {/* <TouchableOpacity style={styles.btn}>
+                    <Text>TESTE</Text>
+                </TouchableOpacity> */}
+
+                <Button>
+                    <Text>Teste111</Text>
+                </Button>
             </View>
         </View>
     )
-
 }
+
+export default LoginView
